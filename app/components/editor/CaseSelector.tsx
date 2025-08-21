803e70c006184d2b1e2e50ef4c8ffb6107206b29
Alvin Quach <alvinwquach@gmail.com>
2025-08-21 09:57:50 -0700

    Remove unused import

--- app/components/editor/CaseSelector.tsx
+++ app/components/editor/CaseSelector.tsx
@@ -1,5 +1,5 @@
 import { useState, useMemo, useCallback } from "react";
-import Select, { SingleValue, StylesConfig } from "react-select";
+import Select, { SingleValue } from "react-select";
 import {
   CaseClause,
   SelectOption,
